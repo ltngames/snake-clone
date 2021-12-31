eed6974dee05a959ca242128c2555b75f42eda16
inc0der <inc0der.dev@gmail.com>
2021-12-31 12:41:27 -0400

    Add rotation for snake segments

--- src/entities/Body.hx
+++ src/entities/Body.hx
@@ -1,5 +1,7 @@
 package entities;
 
+import hxd.Math;
+import h2d.RenderContext;
 import hxd.Direction;
 import core.Sprite;
 
@@ -10,5 +12,19 @@ class Body extends Sprite {
     var tile = Res.img.snake_body.toTile();
     tile.setCenterRatio();
     super(x, y, tile, false, parent);
+  }
+
+  public override function sync(ctx: RenderContext) {
+    super.sync(ctx);
+    switch direction {
+      case Up:
+        bitmap.rotation = 0;
+      case Down:
+        bitmap.rotation = 0;
+      case Left:
+        bitmap.rotation = Math.degToRad(-90);
+      case Right:
+        bitmap.rotation = Math.degToRad(90);
+    }
   }
 }
